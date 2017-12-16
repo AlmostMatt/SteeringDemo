e1f50ac6f2203dc866b45cb9f5083b6bf71c3c17
Matthew Hyndman <myndman@gmail.com>
2017-12-16 01:25:23 -0800

    Unity misc

---
+++ Assets/Unit.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour {
+	void Start () {
+		Steering steering = GetComponent<Steering>();
+		steering.setSpeed(5f, 20f);
+		steering.addBehaviour(1f, new Wander());
+	}
+}
